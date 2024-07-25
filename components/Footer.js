64c467f337daa4921d5f39277b0c2ac4fde2427e
bryanstevens314 <b.stevens.photo@gmail.com>
2024-07-25 10:29:37 -0600

    Updated footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2024 GOTSKILL, LLC. All rights reserved.</p>
-      <nav>
-        <a href="/about">About</a>
-        <a href="/help">Help</a>
-        <a href="/privacy">Privacy</a>
-        <a href="/terms">Terms</a>
-      </nav>
-    </footer>
-  );
+    return (
+        <footer class="app-footer text-md-left text-center">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-6 mb-md-0 mb-4">
+                        <ul class="footer-link">
+                            <li>
+                                <a href="/about">About</a>
+                            </li>
+                            <li>
+                                <a href="/help">Help</a>
+                            </li>
+                            <li>
+                                <a href="/privacy">Privacy</a>
+                            </li>
+                            <li>
+                                <a href="/terms">Terms</a>
+                            </li>
+                        </ul>
+                        <p class="copyright">&copy; 2024 GOTSKILL, LLC. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
